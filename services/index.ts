a2e3c00e881d9294cd9ae008d0390322ae1fb0ff
神樹桜乃 <kodama@sakuno.moe>
2021-11-15 23:07:45 +0800

    Update

--- services/index.ts
+++ services/index.ts
@@ -62,12 +62,13 @@ export async function addNotification(data: Array<NotificationDto>) {
     for (const { userId, invitations } of data) {
       const [announcement] = await connection.execute<OkPacket>("INSERT INTO announcement(sender, title, content, inform_instant) VALUES('admin', ?, ?, 0);", [
         "你得到了 Meta Network 空投邀请码",
-        `Hi，Matataki 的老朋友！<br>` +
-        `还记得那句我们的 Slogan 吗？“发布瞬间，灵感永存”——在实现了去中心化存储、完成了社交代币初体验之后，我们决定用一个新的产品来帮助热情的创造者开启 Web3.0 独立站点！无需中心平台、可以用 Meta 域名直接访问的 Meta Space，将以社交代币彼此联结，产生价值，组成 Meta Network！<br>` +
-        `您可以在一个真正意义上自己的站点上，来体验到以往必须通过 Matataki 这样的平台来实现的功能。现在前往 <a href="https://home.metanetwork.online/">Meta Network</a>，就可以通过您的邀请码：<br>` +
-        invitations.join("<br>") + "<br>" +
-        `来探索下一代的社交网络——一个个信息星球组成的数字星系，Meta Network！<br>` +
-        `<a href="https://home.metanetwork.online/">https://home.metanetwork.online/</a><br>`,
+        "Hi，Matataki 的老朋友！<br>还记得那句我们的 Slogan 吗？<br><strong>“发布瞬间，灵感永存!”</strong><br><br>" +
+        "——在实现了去中心化存储、完成了社交代币初体验之后，<br>我们决定用一个新的产品，来帮助热情的创造者开启 Web3.0 独立站点！<br><br>" +
+        "无需中心平台、可以用 Meta 域名直接访问的 Meta Space，<br>将以社交代币彼此联结，产生价值，组成 Meta Network！<br><br>" +
+        '<strong>现在，我们就邀请您前往 <a href="https://home.metanetwork.online/">Meta Network</a>，通过您的邀请码来注册账户：</strong><br>' +
+        invitations.join("<br>") + "<br><br>" +
+        "您可以在一个真正意义上自己的站点上，<br>来体验到以往必须通过 Matataki 这样的平台来实现的功能。<br>和老朋友们一起探索下一代的社交网络——一个个信息星球组成的数字星系，Meta Network！<br><br>" +
+        '<a href="https://home.metanetwork.online/">https://home.metanetwork.online/</a><br>',
       ]);
       const [event] = await connection.execute<OkPacket>("INSERT INTO notify_event(user_id, action, object_id, object_type, create_time) VALUES(?, 'annouce', ?, 'announcement', NOW());", [
         userId, announcement.insertId,
